2b22259ea83a77768101d880d7c4e0edb1c8c1bc
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2021-12-24 23:10:53 +0530

    Add files via upload

---
+++ _10_Advanced_Patterns.java
@@ -0,0 +1,70 @@
+import com.sun.tools.javac.Main;
+
+public class _10_Advanced_Patterns {
+    public static void main(String[] args) {
+
+//        Q.1 Butterfly patterns
+
+//        *             *
+//        * *         * *
+//        * * *     * * *
+//        * * * * * * * *
+//        * * * * * * * *
+//        * * *     * * *
+//        * *         * *
+//        *             *
+//   int n=5;
+//   for (int i=1;i<=n;i++){
+//       for (int j=1;j<=i;j++){
+//       System.out.print("*");
+//   }
+//   int spaces =2*(n-i);
+//   for(int j=1; j<=spaces;j++){
+//       System.out.print(" ");
+//   }
+//   for (int j=1;j<=i;j++){
+//       System.out.print("*");
+//   }
+//       System.out.println();
+//
+//    }
+//   //lower
+//        for (int i=n;i>=1;i--){
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            int spaces =2*(n-i);
+//            for(int j=1; j<=spaces;j++){
+//                System.out.print(" ");
+//            }
+//            for (int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+
+
+
+
+//        Q.2 Solid rhombus
+
+//                              *****
+//                             *****
+//                            *****
+//                           *****
+//                          *****
+//
+//        int n=5;
+//        for (int i=1;i<=n;i++){
+//            for (int j=1;j<=n-i;j++){
+//                System.out.print( " ");
+//            }
+//            for (int j=1;j<=5;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+}
+}
