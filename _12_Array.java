2a8bae2ff27c7dde2680ad43e9a7ccd8c9d20e0b
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-01-10 13:41:35 +0530

    Add files via upload

---
+++ _12_Array.java
@@ -0,0 +1,16 @@
+public class _12_Array {
+    public static void main(String[] args) {
+        int [] mark = new int[3];
+        mark[0]=12;
+        mark[1]=23;
+        mark[2]=15;
+//        System.out.println(mark[0]);
+//        System.out.println(mark[1]);
+//        System.out.println(mark[2]);
+
+for (int i=0;i<3;i++){
+    System.out.println(mark[i]);
+}
+
+    }
+}
